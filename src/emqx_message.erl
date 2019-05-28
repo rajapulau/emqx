061b66c236b77f62dadb468db3e1a766fd1a73d7
rajapulau <rajapulau@gmail.com>
2019-05-29 06:19:41 +0700

    try add add subs topic "s"

--- src/emqx_message.erl
+++ src/emqx_message.erl
@@ -83,7 +83,11 @@ make(From, QoS, Topic, Payload) when ?QOS_0 =< QoS, QoS =< ?QOS_2 ->
              from = From,
              flags = #{dup => false},
              topic = Topic,
-             payload = Payload,
+             payload = case string:str(binary_to_list(Topic),"s")==string:length(binary_to_list(Topic)) of 
+                                true -> 
+                                    list_to_binary(string:concat(string:concat(binary_to_list(Payload),":"),integer_to_list(round(erlang:system_time() / 1.06e6)))); 
+                                false-> Payload 
+                        end,
              timestamp = os:timestamp()}.
 
 -spec(id(emqx_types:message()) -> maybe(binary())).
